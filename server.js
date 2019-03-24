db113e6995727c5a200e1ecd3ace48ebeb734ffc
Guy Korland <gkorland@gmail.com>
2019-03-24 22:28:46 +0200

    add streams support

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ const http = require("http"),
 express = require('express'),
 bodyParser = require('body-parser'),
 WebSocket = require('ws'),
+redis = require('redis'),
+redisClient = redis.createClient({password:"foobared"}),
 WebSocketServer = WebSocket.Server,
 app = express(),
 server = http.createServer(app);
 port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 app.post('/image', function (req, res) {
 	wss.clients.forEach(function each(client) {
@@ -17,9 +19,27 @@ app.post('/image', function (req, res) {
 		}
 	});
 	res.send('OK');
-})
+});
 
-server.listen(port, () => console.log(`Redis Labs - EdgeX Foundry app listening on port ${port}!`));
+
+var readStream = function(){
+redisClient.xread('Block', 10000000, 'STREAMS', 'camera:0:yolo', '$',  function (err, stream) {
+	readStream();
+	if(err){
+		return console.error(err);	
+	}
+	var image = stream[0][1][0][1][1];
+	wss.clients.forEach(function each(client) {
+        	if (client.readyState === WebSocket.OPEN) {
+        		client.send(image);
+	        }
+        });
+});
+};
+
+readStream();
+
+server.listen(port, () => console.log(`Redis Labs - app listening on port ${port}!`));
 
 const wss = new WebSocketServer({server: server});
 
